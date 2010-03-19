5e55a606bd5bbb16ceb2492d1a4e8545c8954ff5
stephane <stephane@0a187509-3938-4927-b436-45edae4875fc>
2010-03-19 16:36:01 +0000

    Created class to handle UDP reception
    Created a class to create an UDP socket that handles the reception of
    packets of different data types
    
    git-svn-id: http://svn.med.ira.inaf.it/repos/spectra2@12 0a187509-3938-4927-b436-45edae4875fc

--- src/server.cc
+++ src/server.cc
@@ -1,30 +1,62 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
+#include <ipp.h>
 
 using boost::asio::ip::udp;
+
+template<class T>
+class udp_sock
+{
+    public:
+        udp_sock(std::string host, unsigned short port);
+        T *read();
+    private:
+        boost::asio::io_service _io_service;
+        udp::socket _sock;
+        boost::array<T,256> _buf;
+};
+
+template<class T> udp_sock<T>::udp_sock(std::string host = "localhost", unsigned short port = 50000) : _sock(_io_service)
+{
+    udp::resolver res(_io_service);
+    udp::resolver::query query(udp::v4(), host, "50000");
+    udp::endpoint ep = *res.resolve(query);
+    ep.port(port);
+    _sock.open(udp::v4());
+    _sock.bind(ep);
+}
+
+template<class T> T *udp_sock<T>::read()
+{
+    udp::endpoint remote;
+    boost::system::error_code error;
+    _sock.receive_from(boost::asio::buffer(_buf), remote, 0, error);
+
+    if (error && error != boost::asio::error::message_size)
+    {
+        throw boost::system::system_error(error);
+    }
+
+    return _buf.data();
+}
 
 int main(int argc, char **argv)
 {
     try {
-        boost::asio::io_service io_serv;
-        udp::socket socket(io_serv, udp::endpoint(udp::v4(), 50000));
+
+        udp_sock<Ipp16s> s("localhost", 50000);
 
         while(true)
         {
-            boost::array<char, 256> buf;
-            udp::endpoint remote;
-            boost::system::error_code error;
-            socket.receive_from(boost::asio::buffer(buf), remote, 0, error);
-
-            if (error && error != boost::asio::error::message_size)
-            {
-                throw boost::system::system_error(error);
-            }
+            Ipp16s *d;
+            d = s.read();
+            std::cout << (char *)d;
+            std::cout.flush();
         }
     }
     catch( std::exception &e )
     {
-        std::cerr << e.what() << std::endl;
+        std::cerr << "Ex: " << e.what() << std::endl;
     }
 }
