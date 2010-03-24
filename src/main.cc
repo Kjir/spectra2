7f50fc0bcc36ef9863f6664cc79e7e8d063522e6
stephane <stephane@0a187509-3938-4927-b436-45edae4875fc>
2010-03-24 12:32:04 +0000

    Implemented buffer to handle concurrent transformations
    git-svn-id: http://svn.med.ira.inaf.it/repos/spectra2@22 0a187509-3938-4927-b436-45edae4875fc

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <boost/threadpool.hpp>
 #include <boost/bind.hpp>
+#include <boost/array.hpp>
 #include <ipp.h>
 #include "server.hpp"
 #include "fft.hpp"
+
+#define MAX_ITER 5
 
 int main(int argc, char **argv)
 {
@@ -11,21 +14,36 @@ int main(int argc, char **argv)
     try {
 
         boost::threadpool::pool tp(5);
-        int i = 1;
-        Ipp16s *src, *dst;
+        int i = 0;
+        int siglen = fft::order_to_length(order);
+        boost::array<Ipp16s *, MAX_ITER> src, dst;
         IppsFFTSpec_R_16s *spec = fft::allocSpec(&spec, order);
 
-        dst = fft::alloc(dst, fft::order_to_length(order));
-        fft::zero_mem(dst, fft::order_to_length(order));
+        for( int k = 0; k < MAX_ITER; k++) {
+            dst[k] = fft::alloc(dst[k], siglen);
+            fft::zero_mem(dst[k], siglen);
+        }
         fft f(spec);
         udp_sock<Ipp16s> s("localhost", 50000);
 
-        while(true)
+        for(i = 0; i < MAX_ITER; i++)
         {
-            src = s.read();
+            std::cerr << "Reading: " << std::endl;
+            src[i] = s.read();
 
-            tp.schedule(boost::bind(&fft::transform, &f, src, dst, order, 1, 12));
+            tp.schedule(boost::bind(&fft::transform, &f, src[i], dst[i], order, 1, 12));
         }
+
+        std::cerr << "Waiting..." << std::endl;
+        tp.wait();
+        std::cerr << "...done!" << std::endl;
+
+        for(i=0; i < MAX_ITER; i++)
+        {
+            std::cout.write((char *)src[i], sizeof(src) * 9000);
+            std::cout << "break" << std::endl;
+        }
+        std::cout.flush();
     }
     catch( std::exception &e )
     {
